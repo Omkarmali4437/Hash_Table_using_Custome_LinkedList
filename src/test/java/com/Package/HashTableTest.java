508c7abebc15ee6bb86aa868917a62e3be6dc608
omkarmali4437 <omkarmalli688@gmail.com>
2021-03-15 22:31:34 +0530

    Added logic for counting occurrence of words in a Paragraph

--- src/test/java/com/Package/HashTableTest.java
+++ src/test/java/com/Package/HashTableTest.java
@@ -6,11 +6,9 @@ import org.junit.Test;
 public class HashTableTest {
 
     @Test
-    public void find_frequency_of_given_words(){
-
+    public void find_frequency_of_given_sentence(){
         String sentence="To be or not to be";
         HashTable<String,Integer> hashTable=new HashTable<>();
-
         String[] words=sentence.toLowerCase().split(" ") ;
 
         for(String word : words){
@@ -24,6 +22,32 @@ public class HashTableTest {
             hashTable.add(word,value);
         }
         int frequency = hashTable.get("be");
+        System.out.println(hashTable);
+        Assert.assertEquals(2, frequency);
+    }
+
+    @Test
+    public void find_frequency_of_given_paragraph(){
+        String para="Paranoids are not"+
+            "paranoid because they are paranoid but"+
+            "because they keep putting themselves"+
+            "deliberately into paranoid avoidable"+
+            "situations";
+
+        HashTable<String,Integer> hashTable=new HashTable<>();
+        String[] words=para.toLowerCase().split(" ") ;
+
+        for(String word : words){
+            Integer value=hashTable.get(word);
+            if(value == null){
+                value=1;
+            }
+            else {
+                value=value+1;
+            }
+            hashTable.add(word,value);
+        }
+        int frequency = hashTable.get("are");
         System.out.println(hashTable);
         Assert.assertEquals(2, frequency);
     }
