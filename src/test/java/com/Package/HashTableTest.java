a053a8b50297a70afd0b40e852ab98a2c97bd581
omkarmali4437 <omkarmalli688@gmail.com>
2021-03-15 22:42:08 +0530

    Added logic for deleting a word from a paragraph

--- src/test/java/com/Package/HashTableTest.java
+++ src/test/java/com/Package/HashTableTest.java
@@ -3,6 +3,8 @@ package com.Package;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.LinkedHashMap;
+
 public class HashTableTest {
 
     @Test
@@ -50,5 +52,31 @@ public class HashTableTest {
         int frequency = hashTable.get("are");
         System.out.println(hashTable);
         Assert.assertEquals(2, frequency);
+    }
+
+    @Test
+    public void delete_word_from_given_paragraph(){
+        String para="Paranoids are not"+
+                "paranoid because they are paranoid but"+
+                "because they keep putting themselves"+
+                "deliberately into paranoid avoidable"+
+                "situations";
+
+        LinkedHashMap<String,Integer> hashTable= new LinkedHashMap<>();
+        String[] words=para.toLowerCase().split(" ") ;
+
+        for(String word : words){
+            Integer value=hashTable.get(word);
+            if(value == null){
+                value=1;
+            }
+            else {
+                value=value+1;
+            }
+        }
+        System.out.println(hashTable);
+        hashTable.remove("avoidable");
+        System.out.println(hashTable);
+        Assert.assertEquals(null,hashTable.get("avoidable"));
     }
 }
