7de749d7cfda3fa7f6289575cbf7479deed1c687
omkarmali4437 <omkarmalli688@gmail.com>
2021-03-15 22:21:59 +0530

    Created logic for creating Hash table and added logic for finding frequency of words in a sentence

---
+++ src/main/java/com/Package/HashTable.java
@@ -0,0 +1,35 @@
+package com.Package;
+
+public class HashTable <T extends Comparable,V extends Comparable>{
+    LinkedList<T> linkedList;
+
+    public HashTable(){
+        this.linkedList=new LinkedList<>();
+    }
+
+    public V get(T data){
+        Node<T,V> node= this.linkedList.searchNode(data);
+        if(node == null){
+            return null;
+        }
+        else{
+            return node.getValue();
+        }
+    }
+
+    public void add(T data,V value){
+        Node<T,V> node=this.linkedList.searchNode(data);
+        if(node == null){
+            node=new Node(data,value);
+            this.linkedList.insertatBack(node);
+        }
+        else{
+            node.setValue(value);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "HashTable{" + linkedList + '}';
+    }
+}
